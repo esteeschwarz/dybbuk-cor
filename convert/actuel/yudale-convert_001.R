76b54dfd7accfa1318f912a2f9831c80ba4453b6
estee <91368979+esteeschwarz@users.noreply.github.com>
2024-08-05 18:57:30 +0200

    fin, upd.

--- convert/actuel/yudale-convert_001.R
+++ convert/actuel/yudale-convert_001.R
@@ -21,7 +21,8 @@ path.dir<-"~/Documents/GitHub/dybbuk-cor/convert/actuel/TEI"
 #path.dir<-"https://raw.githubusercontent/esteeschwarz/dybbuk-cor/main/convert/actuel/TEI"
 chose.file<-""
 chose.file<-"yudale_ezd_pre_semicor_003.txt" # last used working version
-chose.file<-"yudale_ezd_pre_semicor_003STfwd.txt" # forward edited version 
+#chose.file<-"yudale_ezd_pre_semicor_003STfwd.txt" # forward edited version 
+### > chose finalised .xml output file at script bottom
 path.chose.file<-paste(path.dir,chose.file,sep = "/")
 path.chose.file
 if (chose.file!="")
