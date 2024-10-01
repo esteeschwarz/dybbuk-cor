1eefb2d3eb8ad91060bef33dc01e9cdf7ad18394
esteeschwarz <91368979+esteeschwarz@users.noreply.github.com>
2024-10-01 17:19:30 +0200

    25

--- convert/actuel/hello.py
+++ convert/actuel/hello.py
@@ -1,1 +1,8 @@
 print("Hello, World!")
+
+# Open the file in write mode (this will create the file if it doesn't exist)
+with open('py-output.md', 'w') as file:
+    # Write a line of text to the file
+    file.write('1. This is a line of text.\n')
+
+print("file written...")
---
+++ convert/actuel/yudale-convert_002.R
@@ -0,0 +1,10 @@
+# 14404.git workflow adaptation, ezdrama processing
+# 20241001(17.07)
+# essai TEI refactoring analogue <yudale-convert_001.R> via
+# automated git workflow (git action)
+#####################################
+#####################################
+# test routine
+# call python script from R
+system("python hello.py ")
+print("python script called, file written?")
