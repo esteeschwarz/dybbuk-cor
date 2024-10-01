8964a06188a68d6ff16e4336ad5d02b890f7bdfc
esteeschwarz <91368979+esteeschwarz@users.noreply.github.com>
2024-10-01 10:49:26 +0200

    21

--- convert/actuel/parser.git.py
+++ convert/actuel/parser.git.py
@@ -532,6 +532,6 @@ class Parser():
 
 if __name__ == "__main__":
     parser = Parser()
-    parser.process_file(sys.argv[1])
-#    parser.process_file("TEI/yudale_ezd_pre_semicor_003.txt")
+#    parser.process_file(sys.argv[1])
+    parser.process_file("TEI/yudale_ezd_pre_semicor_003.txt")
    # parser.process_file(r.qfile)
