72dd0ebc25b833391751a06622421f24b54233cd
esteeschwarz <91368979+esteeschwarz@users.noreply.github.com>
2024-10-02 08:35:30 +0200

    34

--- convert/actuel/test-r.R
+++ convert/actuel/test-r.R
@@ -3,5 +3,6 @@ test<-"testartifact"
              writeLines(test,"./testart.md")
              print(list.files("."))
           #   source("test-r.R")
-.libPaths()
-try(library(xml2)) # test libdir
+print(.libPaths())
+
+try(library(xml2,lib.loc = "./rlibs")) # test libdir
