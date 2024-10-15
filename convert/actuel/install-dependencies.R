f3fc07b37fcfc7e735f2e65cc59e9ea36326e34d
esteeschwarz <91368979+esteeschwarz@users.noreply.github.com>
2024-10-15 18:17:17 +0200

    redo dependency script and workflow for no cache run

--- convert/actuel/install-dependencies.R
+++ convert/actuel/install-dependencies.R
@@ -4,22 +4,34 @@ cat(">>>>>> rlibs dir:\n")
 libs<-list.files("rlibs")
 print(libs)
 
-dep<-read.csv("convert/actuel/rdependencies.csv")
-
+#dep<-read.csv("convert/actuel/rdependencies.csv")
+extract_libraries <- function(file) {
+  lines <- readLines(file)
+  libraries <- unique(gsub(".*library\\((.*)\\).*", "\\1", grep("library\\(", lines, value = TRUE)))
+  print(libraries)
+  return(libraries)
+}
+libraries<-extract_libraries("convert/actuel/yudale-convert_002.R")
 rlib<-"rlibs"
 repos<-'https://cloud.r-project.org'
-dep.array<-dep$pkg[dep$chk!=1]
-m<-dep.array%in%libs
-dep.to.install<-dep.array[!m]
+#dep.array<-dep$pkg[dep$chk!=1]
+dep.array<-libraries
+dep.to.install<-dep.array
+if(length(libs)>0){
+  m<-dep.array%in%libs
+  dep.to.install<-dep.array[!m]
+}
 cat(">>>>>> libs to install:\n")
 print(dep.to.install)
+#print(dep.to.install)
 dep.array<-dep.to.install
+#dep.array<-libraries
 for(k in 1:length(dep.array)){
   li<-dep.array[k]
   cat(">>>>>>>> library to install:",li,"\n")
   install.packages(li,lib=rlib,repos=repos)
   cat(">>>>>>>> library installed\n")
-  dep$chk[dep$pkg==li]<-1
+  #dep$chk[dep$pkg==li]<-1
 }
 #dep$chk[4]<-1
 #write.csv(dep,"rdependencies.csv",row.names = F)
